0630734ec8949a2f0628895ce2db3ea9cceb2493
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2023-10-30 10:58:14 +0700

    penghapusan gambar utama, penggantian teks konten

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
     const fixedNav = header.offsetTop;
     const toTop = document.querySelector('#to-top');
 
-    if(window.pageYOffset > fixedNav) {
+    if(window.scrollY > fixedNav) {
         header.classList.add('navbar-fixed');
         toTop.classList.remove('hidden');
         toTop.classList.add('flex');
@@ -25,16 +25,6 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
-
-
-// Klik di luar hamburger
-window.addEventListener('click', function(e) {
-    if(e.target != hamburger && e.target != navMenu) {
-        hamburger.classList.remove('hamburger-active');
-        navMenu.classList.toggle('hidden');
-    }
-})
-
 
 
 // Darkmode toggle
@@ -58,3 +48,50 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   } else {
     darkToggle.checked = false;
   }
+
+
+// kirim pesan
+const containerContact = document.querySelector('#containerContact');
+const nama = containerContact.querySelector('#name');
+const email = containerContact.querySelector('#email');
+const message = containerContact.querySelector("#message");
+const buttonSend = containerContact.querySelector("#buttonSend")
+buttonSend.addEventListener('click', function(e){
+    let userName;
+    let userEmail;
+    let userMessage;
+    let hasError = false;
+
+    if(nama.value == ""){
+        containerContact.querySelector('.alert-name').classList.remove('hidden');
+        hasError = true;
+    } else {
+        containerContact.querySelector('.alert-name').classList.add('hidden');
+        userName = nama.value;
+    }
+
+    if(email.value == ""){
+        containerContact.querySelector('.alert-email').classList.remove('hidden');
+        hasError = true;
+
+    } else {
+        containerContact.querySelector('.alert-email').classList.add('hidden');
+        userEmail = email.value;
+    }
+
+    if(message.value == ""){
+        containerContact.querySelector('.alert-message').classList.remove('hidden');
+        hasError = true;
+
+    } else {
+        containerContact.querySelector('.alert-message').classList.add('hidden');
+        userMessage = message.value;
+    }
+
+    if(!hasError){
+        const whatsappURL = `https://api.whatsapp.com/send/?phone=6282264812480&text=nama%20:%20${userName}%0Aemail%20:%20${userEmail}%0Apesan%20:%20${userMessage}`
+        window.open(whatsappURL, '_blank');
+    } else {
+        e.preventDefault();
+    }
+})
